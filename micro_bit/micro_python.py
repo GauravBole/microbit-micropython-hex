bacb2bdfe1b3451afbc94503a1626e1b0dbdc75d
Gaurav Bole <gaurav.bole@bulkmro.com>
2020-08-15 19:28:15 +0530

    tested final hex conveter

---
+++ micro_bit/micro_python.py
@@ -0,0 +1,12 @@
+from microbit import *
+import time 
+import math 
+import sys 
+import machine 
+import music 
+import random
+
+if True :
+  display.show(Image.HEART)
+  sleep(10000)
+  display.scroll(12)
